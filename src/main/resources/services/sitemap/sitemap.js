0e1efb63fe3e9bd5c8d2b68bf8f7398f21774527
Bobby <bellfalasch@gmail.com>
2017-05-16 11:36:42 +0200

    Refactoring and comments

--- src/main/resources/services/sitemap/sitemap.js
+++ src/main/resources/services/sitemap/sitemap.js
@@ -1,11 +1,16 @@
 var libs = {
     portal: require('/lib/xp/portal'),
     content: require('/lib/xp/content'),
-    util : require('/lib/enonic/util'),
+    util: require('/lib/enonic/util'),
     xslt: require('/lib/xp/xslt')
 };
 
-var view = resolve('sitemap.xsl');
+var globals = {
+	view: resolve('sitemap.xsl'),
+	updatePeriod: "monthly",
+	priority: "0.5",
+	alwaysAdd: "portal:site"
+}
 
 function handleGet(req) {
 
@@ -13,30 +18,32 @@ function handleGet(req) {
     var siteConfig = libs.portal.getSiteConfig();
 
     var arrContentTypes = [];
-    var siteMapSettings = siteConfig.siteMap ? libs.util.data.forceArray(siteConfig.siteMap) : null;
-    var changefreq = {}, priority = {};
+	 var changefreq = {}, priority = {};
 	 var siteAdded = false;
+    var siteMapSettings = siteConfig.siteMap ? libs.util.data.forceArray(siteConfig.siteMap) : null;
     if (siteMapSettings) {
         for (var j = 0; j < siteMapSettings.length; j++) {
             var cty = siteMapSettings[j].contentType || "";
-				if (cty === 'portal:site') { siteAdded = true; } // To be able to automatically add site content type if not already added by user.
+				if (cty === globals.alwaysAdd) { siteAdded = true; log.info("site added by user"); } // To be able to automatically add site content type if not already added by user.
 				arrContentTypes.push(cty);
-            changefreq[cty] = siteMapSettings[j].updatePeriod || "monthly";
-            priority[cty] = siteMapSettings[j].priority || "0.5";
+            changefreq[cty] = siteMapSettings[j].updatePeriod || globals.updatePeriod;
+            priority[cty] = siteMapSettings[j].priority || globals.priority;
         }
     }
+
 	 // Default settings for site (frontpage) set to be changed and prioritized often.
 	 if (!siteAdded) {
-		 cty = 'portal:site';
+		 cty = globals.alwaysAdd;
 		 arrContentTypes.push(cty);
 		 changefreq[cty] = "Hourly";
 		 priority[cty] = "1.0";
+		 log.info("site added from code");
 	 }
 
-	 // Only allow content from current Site to populate the RSS feed.
+	 // Only allow content from current Site to populate the sitemap.
 	 var folderPath = site._path;
 	 var contentRoot = '/content' + folderPath + '/';
-	 var query = '_path LIKE "' + contentRoot + '*"';
+	 var query = '_path LIKE "' + contentRoot + '*" OR _path = "' + contentRoot + '"';
 
     var queryRes = libs.content.query({
         query: query,
@@ -45,6 +52,7 @@ function handleGet(req) {
         count: 1000,
     });
 
+	 // Go through the results and add the corresponding settings for each match.
     for(var i = 0 ; i < queryRes.hits.length; i++ ) {
         if(queryRes.hits[i].type){
             queryRes.hits[i].changeFreq = changefreq[queryRes.hits[i].type];
@@ -60,7 +68,7 @@ function handleGet(req) {
 
     return {
         contentType: 'text/xml',
-        body: libs.xslt.render(view, model)
+        body: libs.xslt.render(globals.view, model)
     };
 }
 exports.get = handleGet;
