c6fdd96af8f3650f47a0ac4c089fcf0538930f4e
alansemenov <ase@enonic.com>
2023-06-23 14:47:54 +0200

    Configurable limit of included items #52

--- src/main/resources/services/sitemap/sitemap.js
+++ src/main/resources/services/sitemap/sitemap.js
@@ -13,14 +13,14 @@ var globals = {
 };
 
 function handleGet(req) {
-
     var site = libs.portal.getSite();
     var siteConfig = libs.portal.getSiteConfig();
 
     var arrContentTypes = [];
-	 var changefreq = {}, priority = {};
-	 var siteAdded = false;
+    var changefreq = {}, priority = {};
+    var siteAdded = false;
     var siteMapSettings = siteConfig.siteMap ? libs.util.data.forceArray(siteConfig.siteMap) : null;
+    const maxItems = siteConfig.maxItems || 10000;
     if (siteMapSettings) {
         for (var j = 0; j < siteMapSettings.length; j++) {
             var cty = siteMapSettings[j].contentType || "";
@@ -31,41 +31,41 @@ function handleGet(req) {
         }
     }
 
-	 // Default settings for site (frontpage) set to be changed and prioritized often.
-	 if (!siteAdded) {
-		 cty = globals.alwaysAdd;
-		 arrContentTypes.push(cty);
-		 changefreq[cty] = "Hourly";
-		 priority[cty] = "1.0";
-	 }
+    // Default settings for site (frontpage) set to be changed and prioritized often.
+    if (!siteAdded) {
+        cty = globals.alwaysAdd;
+        arrContentTypes.push(cty);
+        changefreq[cty] = "Hourly";
+        priority[cty] = "1.0";
+    }
 
-	 // Only allow content from current Site to populate the sitemap.
-	 var folderPath = site._path;
-	 var contentRoot = '/content' + folderPath + '';
-	 var query = '_path LIKE "' + contentRoot + '/*" OR _path = "' + contentRoot + '"';
+    // Only allow content from current Site to populate the sitemap.
+    var folderPath = site._path;
+    var contentRoot = '/content' + folderPath + '';
+    var query = '_path LIKE "' + contentRoot + '/*" OR _path = "' + contentRoot + '"';
 
-	 // Query that respects the settings from SEO Metafield plugin, if present, using 6.10 query filters - @nerdegutt.
+     // Query that respects the settings from SEO Metafield plugin, if present, using 6.10 query filters - @nerdegutt.
     var result = libs.content.query({
         query: query,
         sort : 'modifiedTime DESC',
         contentTypes: arrContentTypes,
-        count: 10000,
-		  filters: {
-		    boolean: {
-		      mustNot: {
-		        hasValue: {
-		          field: "x.com-enonic-app-metafields.meta-data.blockRobots",
-		          values: "true"
-				  }
-		      }
-			 }
-		  }
+        count: maxItems,
+        filters: {
+            boolean: {
+                mustNot: {
+                    hasValue: {
+                        field: "x.com-enonic-app-metafields.meta-data.blockRobots",
+                        values: "true"
+                    }
+                }
+            }
+        }
     });
 
 	 // Go through the results and add the corresponding settings for each match.
-	 var items = [];
+    var items = [];
     for(var i = 0 ; i < result.hits.length; i++ ) {
-		  var item = {};
+        var item = {};
         if (result.hits[i].type) {
             item.changeFreq = changefreq[result.hits[i].type];
             item.priority = priority[result.hits[i].type];
