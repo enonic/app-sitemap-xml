c69749f3a2eb30e4b9efa2f1ac14195c4f90d8d5
Christian Westgaard <comlock@gmail.com>
2024-02-07 16:54:17 +0100

    wip #63 Extend Guillotine

---
+++ src/main/resources/guillotine/getSiteConfigFromSite.ts
@@ -0,0 +1,32 @@
+import type {Site} from '@enonic-types/lib-content';
+import type {SitemapXmlSiteConfig} from './SitemapXmlSiteConfig';
+
+
+import {forceArray} from '@enonic/js-utils/array/forceArray';
+
+
+// Find the site config even when the context is not known.
+export const getSiteConfigFromSite = ({
+	applicationKey, // Avoid app.name so it can be used in Guillotine Extension Context
+	site
+}: {
+	applicationKey: string
+	site: Site<SitemapXmlSiteConfig>,
+}
+) => {
+	// Code courtesy of PVMerlo at Enonic Discuss - https://discuss.enonic.com/u/PVMerlo
+	if (site) {
+		if (site.data) {
+			if (site.data.siteConfig) {
+				const siteConfigs = forceArray(site.data.siteConfig);
+				let siteConfig: Partial<typeof siteConfigs[0]> = {};
+				siteConfigs.forEach((cfg) => {
+					if (cfg.applicationKey == applicationKey) {
+						siteConfig = cfg;
+					}
+				});
+				return siteConfig.config;
+			}
+		}
+	}
+};
---
+++ tsup/constants.ts
@@ -0,0 +1,2 @@
+export const DIR_DST = 'build/resources/main';
+export const DIR_SRC = 'src/main/resources';
